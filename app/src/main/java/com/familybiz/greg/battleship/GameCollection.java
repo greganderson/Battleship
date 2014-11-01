f756ef7656b0187a5789c5b39996b07a387f62a7
Greg Anderson <greg@infobytes.com>
2014-11-01 14:21:40 -0600

    Added an inner class called Game that contains all of the pertinent information of a game.

--- app/src/main/java/com/familybiz/greg/battleship/GameCollection.java
+++ app/src/main/java/com/familybiz/greg/battleship/GameCollection.java
@@ -1,5 +1,10 @@
 package com.familybiz.greg.battleship;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameCollection {
 
 	private static GameCollection mInstance = null;
@@ -10,15 +15,39 @@ public class GameCollection {
 		return mInstance;
 	}
 
-	private GameCollection() {}
+	private ArrayList<Game> mGames;
+
+	private GameCollection() {
+		mGames = new ArrayList<Game>();
+	}
+
+	public void addGame(Player.Cell[][] player1, Player.Cell[][] player2, boolean isPlayer1Turn, boolean isDone, Date timeStarted) {
+		mGames.add(new Game(player1, player2, isPlayer1Turn, isDone, timeStarted));
+	}
+
+	public Map<String, Boolean> getListOfGames() {
+		Map<String, Boolean> result = new HashMap<String, Boolean>();
+		for (Game game : mGames)
+			result.put(game.timeStarted.toString(), game.isDone);
+
+		return result;
+	}
 
 
 	private class Game {
+		public Player.Cell[][] player1Cells;
+		public Player.Cell[][] player2Cells;
+		public boolean isPlayer1Turn;
+		public boolean isDone;
+		public Date timeStarted;
 
-		private boolean isDone;
-		private boolean isPlayer1Turn;
-		private Player.Cell[][] player1Cells;
-		private Player.Cell[][] player2Cells;
+		public Game(Player.Cell[][] player1, Player.Cell[][] player2, boolean isPlayer1Turn, boolean isDone, Date timeStarted) {
+			player1Cells = player1;
+			player2Cells = player2;
+			this.isPlayer1Turn = isPlayer1Turn;
+			this.isDone = isDone;
+			this.timeStarted = timeStarted;
+		}
 	}
 
 }
